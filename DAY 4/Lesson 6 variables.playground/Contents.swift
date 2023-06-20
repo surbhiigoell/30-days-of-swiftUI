f8905e97002cae812689e0b9d2386460447ed126
Surbhi Goel <surbhigoel02@gmail.com>
2023-06-20 19:22:09 +0530

    Lesson 6 variables and constants learnt

---
+++ DAY 4/Lesson 6 variables.playground/Contents.swift
@@ -0,0 +1,27 @@
+//variables
+
+var myVar:String = "hello"
+
+var myInt:Int = 100
+
+print(myVar)
+
+print(myInt)
+
+myVar+="world"
+
+myInt-=50
+
+print(myVar)
+
+print(myInt)
+
+//constants
+
+let myConst:Int = 20
+
+//will throw error
+
+myConst = 30
+
+print(myConst)
