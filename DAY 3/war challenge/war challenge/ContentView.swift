6b971fe7c30b2b03890c015089692b712fda1d3f
Surbhi Goel <surbhigoel02@gmail.com>
2023-06-20 16:37:28 +0530

    Daily challenge completed

---
+++ DAY 3/war challenge/war challenge/ContentView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  war challenge
+//
+//  Created by Surbhi Goel on 20/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Image("background")
+                
+            VStack{
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack(alignment: .center){
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                    Image("card4")
+                    Spacer()
+                }
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                Spacer()
+                Image("dealbutton")
+                Spacer()
+                VStack{
+                    HStack{
+                        Spacer()
+                        Text("Player")
+                        Spacer()
+                        Text("CPU")
+                        Spacer()
+                    }
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                    .padding(.bottom)
+                    .font(.title2)
+                    
+                    HStack{
+                        Spacer()
+                        Text("0")
+                        Spacer()
+                        Text("0")
+                        Spacer()
+                    }
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                    .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                }
+                Spacer()
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
