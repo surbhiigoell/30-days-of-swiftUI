1da10e5e0e5fa9a3614b796038740f4a55e7c730
Surbhi Goel <surbhigoel02@gmail.com>
2023-06-30 18:21:17 +0530

    loops in swift

---
+++ DAY 15/Loops.playground/Contents.swift
@@ -0,0 +1,27 @@
+//For - In loop
+
+for index in 0...10 {
+    print(index)
+}
+
+let myArray = [10,9,8,7,6,5,4,3,2,1]
+
+for element in myArray {
+    print(element)
+}
+
+//repeat-while loop
+
+var counter=10
+
+repeat {
+    print(counter)
+    counter-=1
+} while counter>0
+
+//while loop
+
+while counter<10 {
+    print(counter)
+    counter+=1
+}
