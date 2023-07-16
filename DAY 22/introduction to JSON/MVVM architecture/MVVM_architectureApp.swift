2d79bebc0dad677cc4d9834189af58ae31dcd69d
Surbhi Goel <surbhigoel02@gmail.com>
2023-07-16 17:57:49 +0530

    introduction to json

---
+++ DAY 22/introduction to JSON/MVVM architecture/MVVM_architectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_architectureApp.swift
+//  MVVM architecture
+//
+//  Created by Surbhi Goel on 02/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_architectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DAY 22/introduction to JSON/MVVM architecture/Models/Recipe.swift
@@ -0,0 +1,15 @@
+//
+//  Recipe.swift
+//  MVVM architecture
+//
+//  Created by Surbhi Goel on 02/07/23.
+//
+
+import Foundation
+
+struct Recipe : Identifiable {
+    
+    var id = UUID()
+    var name = ""
+    var cuisine = ""
+}
